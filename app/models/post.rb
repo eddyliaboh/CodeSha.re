40e63edda6627887ea33893fdf9b66035c8239d4
Eddy L <devredant@gmail.com>
2012-03-28 08:31:44 -0600

    Added carrierwave for future ideas and did some housekeeping

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ActiveRecord::Base
 	SYNTAX=['Ruby','C++', 'Java', 'C', 'C#', 'CSS', 'HTML', 'Plain text']
 	LISTING=['Listed', 'Unlisted']
+	
   validates :name,  :presence => true
+
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
- 
-  has_many :comments
 
+  has_many :comments
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120324205828) do
+ActiveRecord::Schema.define(:version => 20120325222227) do
 
   create_table "code_points", :force => true do |t|
     t.integer  "points"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20120324205828) do
     t.string   "location"
     t.string   "syntax"
     t.string   "listing"
+    t.string   "file"
   end
 
   create_table "user_comments", :force => true do |t|
